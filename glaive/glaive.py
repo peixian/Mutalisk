e894051b4ec1ccd472789496dead860fe9cd7d39
Peixian Wang <peixian@fastmail.com>
2016-05-01 21:34:30 -0500

    Add parsing

--- glaive/glaive.py
+++ glaive/glaive.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 import os.path
 import pprint 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def readData(maxReplayCount = 749):
     """reads the data from .arff files (Weka Attribute-Relation files)
@@ -26,6 +28,8 @@ def readData(maxReplayCount = 749):
                 lines = map(lambda x: x.rstrip().split(",")[:2], file.readlines()[11:111])
                 #make and add a dataframe
                 df = pd.DataFrame(lines, columns=("frame", "action"))
+                df = df[df.frame != ""]
+                df["frame"] = df["frame"].map(lambda x: int(x))
                 df["action"] = df["action"].map(lambda x: str(x).replace("Protoss ", "").replace(" ", "_"))
                 for action in list(df["action"].unique()):
                     buildList.add(action)
@@ -37,12 +41,22 @@ def readData(maxReplayCount = 749):
     buildList = list(buildList)
     for df in data:
         df["actionNumber"] = df["action"].map(lambda x: buildList.index(x))
-    return data
+    return data, buildList
     
     
-def createFunctions():
-    """docstring for createFunctions"""
-    pass
+def createVectors(data):
+    """iterates through data and creates a list of vectors of ['frame', 'buildID']
+    refer to buildList for buildID
+    """
+    vec = []
+    for df in data:
+        subset = df[["frame", "actionNumber"]]
+        vec.append(subset.values)
+    vec = [item for sublist in vec for item in sublist]
+    return vec
 
-data = readData()
-print(data)
+data, buildList = readData()
+vectors = createVectors(data)
+plt.scatter(*zip(*vectors))
+plt.show()
+# pprint.pprint(vectors)
